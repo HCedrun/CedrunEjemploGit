646ee9d1dd55391ec17daf3c9b8d5aaac4dfcfe1
damt105 <damt105@36-06.aglinformatica.es>
2022-05-04 20:07:05 +0200

    Versión 2

--- src/cedrunejemplogit/CedrunEjemploGit.java
+++ src/cedrunejemplogit/CedrunEjemploGit.java
@@ -4,7 +4,7 @@ public class CedrunEjemploGit {
 
     public static void main(String[] args) {
         System.out.println("Hola, soy Héctor");
-        System.out.println("Versión 1");
+        System.out.println("Versión 2");
     }
     
 }
